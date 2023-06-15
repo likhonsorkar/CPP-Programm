b0974889f2934d198ccee6d54f0fed9c861fdb3b
unknown <likhonsorkar02@yahoo.com>
2023-06-15 18:17:58 +0600

    Data Type & Variable Assign

---
+++ data-type.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string> // For String;
+using namespace std;
+
+int main(){
+    int a; //int: Stores whole numbers without decimal points;
+    float b; // float: Stores single-precision floating-point numbers with limited decimal precision;
+    double c; // double: Stores double-precision floating-point numbers with higher decimal precision;
+    char d; // char: Stores individual characters from the ASCII character set;
+    string e; // string: Stores sequences of characters as strings;
+    a = 1, b = 2.22, c = 4.4436, d = 'h', e = "String Text";
+    cout<<"Integer Number: "<<a <<endl;
+    cout<<"Float Number: "<<b <<endl;
+    cout<<"Double Number: "<<c <<endl;
+    cout<<"Character: "<<d <<endl;
+    cout<<"String: "<<e <<endl;
+}
