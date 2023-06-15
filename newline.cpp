279858c7b11b4ab9b85d72d39fbac0626e4e0d77
unknown <likhonsorkar02@yahoo.com>
2023-06-15 09:57:06 +0600

    New Line \n / <<endl

--- newline.cpp
+++ newline.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main(){
-    cout << "Hello World!";
-    cout << "Hello Bangladesh!";
-    cout << "Hello People!";
+    //To insert a new line,use \n character
+    cout << "Hello World!\n";
+    // Another way to insert a new line, is with the endl manipulator
+    cout << "Hello Bangladesh!" <<endl;
+    cout << "I am learning C++";
     return 0;
 }
